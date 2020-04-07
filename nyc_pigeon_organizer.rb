724a2d440470e25b6bdb0c428c98d35328b76e30
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-07 22:13:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |k, v|
       v.each do |k2|
-        updated_data << k2 => 
+        updated_data << Hash[k2]
       binding.pry
       end
     end
