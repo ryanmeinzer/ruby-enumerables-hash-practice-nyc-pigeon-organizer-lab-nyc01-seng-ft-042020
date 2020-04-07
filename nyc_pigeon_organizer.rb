4fe3e08d0052c11f561dcf8fdade49c9630cc494
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-07 22:41:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   # HoHoA
   updated_data = {}
-  data.each do |key, color_hash|
+  data.each do |key, value|
     color_hash.each do |color, pigeons_array|
       pigeons_array.each do |pigeon|
         updated_data[pigeon] = color_hash
