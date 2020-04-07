adc20ff89b01df8b43a1e51776a1ae69b92fd8bc
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-07 22:05:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,11 +3,14 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   # HoHoA
+  updated_data = {}
   data.each do |key, value|
     value.each do |k, v|
       v.each do |k2|
+        updated_data << k2 => 
       binding.pry
+      end
     end
   end
-end
+updated_data
 end
