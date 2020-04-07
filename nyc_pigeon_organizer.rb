8ad09f5260bc8ddca6a5242c446313b91a4174e8
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-07 23:23:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
       value_array.each do |name|
         updated_data[name] ||= {}
         updated_data[name][color_gender_location] ||= []
-        updated_data[name][color_gender_location].include?(attributes).push(attributes.to_s)
+        updated_data[name][color_gender_location].push(attributes.to_s)
 # binding.pry
         # if !updated_data[name]
         #   updated_data[name] = {}
